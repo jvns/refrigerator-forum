2cd1a31bb316f8e03fd702de086a11d64eddbd8c
Julia Evans <julia@jvns.ca>
2020-11-09 17:22:31 -0500

    what's a rails session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :posts
   root 'welcome#index'
+  resources :posts
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
