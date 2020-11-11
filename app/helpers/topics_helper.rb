5d33034d9a2ad4061274c745dac392f5965677d1
Julia Evans <julia@jvns.ca>
2020-11-11 17:01:18 -0500

    compress the poems a bit

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,18 @@
 module TopicsHelper
+  def post_box(post)
+    words = JSON.parse(post.text)
+    min_h = words.map {|x| x['top']}.min
+    words = words.map{|w| {
+      top: w['top'] - min_h,
+      left: w['left'],
+      text: w['text'],
+    }}
+
+    max_h = words.map {|x| x[:top]}.max
+    height = [max_h + 30, 300].min
+    {
+      height: height,
+      words: words,
+    }
+  end
 end
