5fa661da80298b48a0156d39258a9ce1a03edb7a
Julia Evans <julia@jvns.ca>
2020-11-11 16:29:41 -0500

    more better login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
+      flash.now[:alert] = "Username or password is invalid"
       render "new"
     end
   end
   def destroy
-    session[:user_id] = nil
+    slibreadline.so.7: cannot open shared object file: No such file or directoryession[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
 end
